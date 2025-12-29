a34896e7a02e02d90e95f05ac719dcaeee2f14d1
EstGui <guilherme.o.esteves@gmail.com>
2025-12-29 15:56:41 -0300

    correção de parametro sendEmail()

--- services/mailer.ts
+++ services/mailer.ts
@@ -2,10 +2,10 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendEmail(content: string, to: string) {
+export async function sendEmail(content: string, to: string = process.env.DESTINATION_EMAIL as string) {
   await resend.emails.send({
     from: "Guilherme Esteves - Portfolio <onboarding@resend.dev>",
-    to: to || process.env.DESTINATION_ADDRESS as string,
+    to,
     subject: "Response to your inquiry",
     text: content,
   });
