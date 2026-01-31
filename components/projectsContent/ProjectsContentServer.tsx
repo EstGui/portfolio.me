5af0aee23d3622602031f9516dfc8d7aed8d02f2
EstGui <guilherme.o.esteves@gmail.com>
2026-01-31 16:49:09 -0300

    Tratamento de exceções e erros ao buscar projetos

--- components/projectsContent/ProjectsContentServer.tsx
+++ components/projectsContent/ProjectsContentServer.tsx
@@ -1,23 +1,32 @@
 import { GithubRepository } from "@/app/types/GithubRepository";
 
 import ProjectsContent from "./ProjectsContent";
+import ProjectsSkeleton from "./ProjectsSkeleton";
 
 // const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default async function ProjectsContentServer() {
   // await delay(3000);
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/github/starred`,
-    {
-      next: { revalidate: 3600 },
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/github/starred`,
+      {
+        next: { revalidate: 3600 },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Erro ao buscar projetos");
     }
-  );
 
-  if (!res.ok) {
-    throw new Error("Erro ao buscar projetos");
+    const data: GithubRepository[] = await res.json();
+
+    if (!Array.isArray(data)) {
+      throw new Error("Erro");
+    }
+
+    return <ProjectsContent repositories={data} />;
+  } catch {
+    return <ProjectsSkeleton />;
   }
-
-  const data: GithubRepository[] = await res.json();
-
-  return <ProjectsContent repositories={data} />;
 }
